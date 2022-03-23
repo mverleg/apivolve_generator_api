91070ebfafd6a8d0fd0b97d728af595f6fc00d1c
Mark <mverleg.noreply@gmail.com>
2022-03-23 23:13:18 +0100

    First few objects for the generation steps

--- src/gen1/steps.rs
+++ src/gen1/steps.rs
@@ -1,14 +1,29 @@
 
 use ::serde::Deserialize;
 use ::serde::Serialize;
+use semver::Version;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct GenerateSteps {
-
+    versions: Vec<(Version, Vec<Step>)>,
+    pending: Vec<Step>,
 }
 
 impl GenerateSteps {
     pub fn new() -> Self {
-        GenerateSteps {}
+        GenerateSteps { versions: vec![], pending: vec![] }
     }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Step {
+    source: Source,
+}
+
+/// The source file and line that created this step.
+/// Intended just for logging and reporting; do not touch the files (which may not be there).
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Source {
+    /// e.g.: v1.2.4/add_user.apiv:10
+    locator: String,
 }
