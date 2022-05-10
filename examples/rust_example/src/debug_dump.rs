24fb34c7fad016d5466940a00001ab770d9d8247
Mark <mverleg.noreply@gmail.com>
2022-05-10 23:18:42 +0200

    First step: accepts config

--- examples/rust_example/src/debug_dump.rs
+++ examples/rust_example/src/debug_dump.rs
@@ -4,11 +4,10 @@ use ::apivolve_generator_api::gen1;
 fn main() {
     env_logger::init();
     assert!(args().skip(1).next().is_none(), "no arguments expected");
-    let config = gen1::AcceptsConfig {};
-    gen1::run_generator(DebugDumpGenerator());
+    let config = gen1::AcceptsConfig {
+        apivolve_version: gen1::Version::new(0, 1, 0),
+        data_structure: gen1::GenerateInputLayout::Steps,
+        encoding: gen1::GenerateInputFormat::Json,
+    };
+    gen1::run_generator(config);
 }
-
-#[derive(Debug)]
-struct DebugDumpGenerator();
-
-impl gen1::Generator for DebugDumpGenerator {}
--- src/gen1/mod.rs
+++ src/gen1/mod.rs
@@ -1,14 +1,13 @@
+pub use self::connect::accepts::AcceptsConfig;
+pub use self::connect::format::GenerateInputFormat;
+pub use self::connect::layout::GenerateInputLayout;
+pub use ::semver::Version;
 
 mod connect;
 
-pub use self::connect::accepts::AcceptsConfig;
-
-pub trait Generator {
-
-}
-
 /// Run the generator, handling the communication with Apivolve.
-pub fn run_generator(generator: impl Generator) {
+pub fn run_generator(accepts_config: AcceptsConfig) {
+
     // eprintln!("start debug_dump code"); //TODO @mark: TEMPORARY! REMOVE THIS!
     // let step_json = serde_json::to_string_pretty(&steps)
     //     .expect("failed to convert apivolve evolution steps to json");
