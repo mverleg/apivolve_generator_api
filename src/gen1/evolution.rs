a0c52313c7cd23efce548f042f9a4dd74d6dcf3e
Mark <mverleg.noreply@gmail.com>
2022-05-10 23:50:54 +0200

    Generation steps and error handling

---
+++ src/gen1/evolution.rs
@@ -0,0 +1,5 @@
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(deny_unknown_fields)]
+pub struct  Evolution {
+}
--- src/gen1/mod.rs
+++ src/gen1/mod.rs
@@ -4,11 +4,20 @@ pub use self::connect::accepts::AcceptsConfig;
 pub use self::connect::format::GenerateInputFormat;
 pub use self::connect::genpref::GenerationPreferences;
 pub use self::connect::layout::GenerateInputLayout;
+pub use self::evolution::Evolution;
 
 mod connect;
+mod evolution;
+
+pub type ErrMsg = String;
+pub type GenResult = Result<(), ErrMsg>;
 
 pub trait Generator {
+    async fn generate_version(&mut self, version: Version, evolution: Evolution) -> GenResult;
 
+    async fn generate_pending(&mut self, evolution: Evolution) -> GenResult;
+
+    async fn finalize(&mut self) -> GenResult;
 }
 
 /// Run the generator, handling the communication with Apivolve.
@@ -17,14 +26,28 @@ pub fn run_generator<G: Generator>(
         make_generator: impl FnOnce(GenerationPreferences) -> G) {
     //TODO @mark: auth
     //TODO @mark: send `accepts_config`
-    let generator_preferences: GenerationPreferences = unimplemented!();
+    let generator_preferences: GenerationPreferences = read_gen_preferences();
     let generator: G = make_generator(generator_preferences);
+    if let Err(err) = generate_until_first_err(generator).await {
+        panic!("{}", err);  //TODO @mark:
+    }
+}
 
+fn generate_until_first_err(generator: G) -> GenResult {
+    while let Some(evolution) = unimplemented!() {
+        let res = generator.generate_version(evolution).await?;
+    }
+    let res = generator.generate_pending(evolution).await;
+    let res = generator.finalize(evolution).await;
+    Ok(())
+}
 
-    // eprintln!("start debug_dump code"); //TODO @mark: TEMPORARY! REMOVE THIS!
-    // let step_json = serde_json::to_string_pretty(&steps)
-    //     .expect("failed to convert apivolve evolution steps to json");
-    // eprintln!("generated debug_dump"); //TODO @mark: TEMPORARY! REMOVE THIS!
-    // println!("{}", step_json);
-    // eprintln!("wrote debug_dump output"); //TODO @mark: TEMPORARY! REMOVE THIS!
+fn read_gen_preferences() -> GenerationPreferences {
+    //TODO @mark: TEMPORARY! REMOVE THIS!
+    if 1==1 { unimplemented!(); }
+    GenerationPreferences {
+        apivolve_version: Version::new(0, 0, 1),
+        output_dir: Default::default(),
+        extra_args: vec![]
+    }
 }
