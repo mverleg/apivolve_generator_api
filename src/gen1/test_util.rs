0236c3959a289755d9020e9ac0350b9ed269cfd1
Mark <mverleg.noreply@gmail.com>
2022-05-12 20:43:07 +0200

    Test function for generator tests

--- src/gen1/test_util.rs
+++ src/gen1/test_util.rs
@@ -6,34 +6,73 @@
 
 use std::path::PathBuf;
 
+fn noop_generator() {}
+
 fn accept_all(_output_dir: &PathBuf) -> Result<(), String> {
     Ok(())
 }
 
-macro_rules! testsuite_full {
-    ($generator_func:ident, $test_func:ident) => {
+// macro_rules! testsuite_full {
+//     ($generator_func:ident, $test_func:ident) => {
+//         #[test]
+//         fn test_no_versions() {
+//             if let Err(err) = test_no_versions(generator_func) {
+//                 panic!("apivolve generator failed: {}", err);
+//             }
+//         }
+//     };
+//     ($generator_func:ident) => {
+//         testsuite_full!($generator_func, accept_all);
+//     };
+// }
+
+pub use testsuite_full;
+use crate::gen1::{AcceptsConfig, GenerationPreferences, Generator};
+
+macro_rules! make_gen_test {
+    ($generator_expr:expr, $test_func:ident) => {
         #[test]
-        fn no_versions() {
+        fn test_no_versions() {
             if let Err(err) = test_no_versions(generator_func) {
                 panic!("apivolve generator failed: {}", err);
             }
         }
     };
-    ($generator_func:ident) => {
-        testsuite_full!($generator_func, accept_all);
-    };
 }
 
-pub use testsuite_full;
-
-pub fn no_versions() {
-
+#[test]
+fn test_generate_no_versions() {
+    if let Err(err) = generate_no_versions(accepts_config, make_generator) {
+        panic!("apivolve generator failed: {}", err);
+    }
 }
 
-pub fn core_features() {
+type GenFn<G: Generator> = impl FnOnce(GenerationPreferences) -> G;
+type TestFn = impl FnOnce(PathBuf);
 
+pub fn generate_no_versions<G: Generator>(
+    accepts_config: AcceptsConfig,
+    make_generator: GenFn<G>,
+    test_func: TestFn,
+) -> Result<(), String> {
+    unimplemented!()
 }
 
-pub fn with_pending() {
-
+pub fn generate_core_features<G: Generator>(
+    accepts_config: AcceptsConfig,
+    make_generator: GenFn<G>,
+    test_func: TestFn,
+) -> Result<(), String> {
+    unimplemented!()
 }
+
+pub fn generate_with_pending<G: Generator>(
+    accepts_config: AcceptsConfig,
+    make_generator: GenFn<G>,
+    test_func: TestFn,
+) -> Result<(), String> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+testsuite_full!(noop_generator);
