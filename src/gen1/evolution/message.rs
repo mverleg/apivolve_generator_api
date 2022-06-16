399bb28258cb97bcb350f2e8b97e0eb72753a43f
Mark <mverleg.noreply@gmail.com>
2022-06-16 22:58:26 +0200

    First attempt at choosing response

--- src/gen1/evolution/message.rs
+++ src/gen1/evolution/message.rs
@@ -1,5 +1,6 @@
 use ::serde::Deserialize;
 use ::serde::Serialize;
+use smallvec::SmallVec;
 use crate::gen1::evolution::Typ;
 
 use crate::gen1::evolution::util::Identifier;
@@ -19,20 +20,33 @@ impl Party {
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(tag = "type", rename_all = "snake_case")]
+//TODO @mark: name
+pub enum ResponseChoice {
+    Optional(SmallVec<[Message; 1]>),
+    Required(SmallVec<[Message; 1]>),
+    //TODO @mark: multiple?
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
 pub struct Message {
+    name: Identifier,
     sender: Party,
     //TODO @mark: e.g. specific client, set of clients or all clients?
     recipient: Party,
     content: Typ,
+    /// One of these messages can be the response; empty means no response,
+    response: ResponseChoice,
 }
 
 impl Message {
-    pub fn new(sender: Party, recipient: Party, content: Typ) -> Self {
+    pub fn new(sender: Party, recipient: Party, content: Typ, response: impl Into<SmallVec<[Message; 1]>>) -> Self {
         Message {
             sender,
             recipient,
             content,
+            response: response.into(),
         }
     }
 }
